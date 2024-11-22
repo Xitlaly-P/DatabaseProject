cdb758310aefcaad004ac853104d4485d13236be
Xitlaly-P <jada23dem@gmail.com>
2024-11-22 18:57:39 -0500

    guests

--- load5rows.sql
+++ load5rows.sql
@@ -2,3 +2,11 @@
 Insert at least 5 rows data in each table via SQL statements. 
 Write SELECT * for each table and do a SNIP or screen shot that shows your tables.
 */
+
+
+---Guests
+INSERT INTO GUEST VALUES('0000001', 'Angel Apellido', 3);
+INSERT INTO GUEST VALUES('0000002', 'Paula Ramos', 2);
+INSERT INTO GUEST VALUES('0000003', 'William George', 1);
+INSERT INTO GUEST VALUES('0000004', 'Benny Barnes', 4);
+INSERT INTO GUEST VALUES('0000005', 'Tiffany Li', 2);
