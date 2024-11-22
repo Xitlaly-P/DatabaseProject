f68cf4ccfed95e1b9376da79fbc5e9518ba517db
DanielMontoya72 <danipacmontoya@gmail.com>
2024-11-22 18:55:40 -0500

    the rooms by tommy wisaeus

--- load5rows.sql
+++ load5rows.sql
@@ -2,3 +2,9 @@
 Insert at least 5 rows data in each table via SQL statements. 
 Write SELECT * for each table and do a SNIP or screen shot that shows your tables.
 */
+
+INSERT INTO ROOM VALUES(101, 100, 'Single Queen', 'Y', 2);
+INSERT INTO ROOM VALUES(202, 150, 'Single King', 'N', 2);
+INSERT INTO ROOM VALUES(303, 200, 'Double Queen', 'N', 4);
+INSERT INTO ROOM VALUES(404, 250, 'Double King', 'Y', 4);
+INSERT INTO ROOM VALUES(505, 500, 'Penthouse', 'Y', 6);
