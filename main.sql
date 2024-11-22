44b0af3d37eb2fc34fba8852c5105b82ea049ccc
Xitlaly-P <jada23dem@gmail.com>
2024-11-22 15:27:03 -0500

    woops

--- main.sql
+++ main.sql
@@ -2,14 +2,14 @@ CREATE TABLE ROOM (
     RoomNo              INT     NOT NULL, 
 	Price               INT     NOT NULL, 
 	RoomType               VARCHAR(15)     NOT NULL, 
-	Accessible                 CHAR(3)         NOT NULL, 
+	Accessible                 BOOLEAN         NOT NULL, 
 	MaxGuests               INT     NOT NULL, 
     PRIMARY KEY (RoomNo) 
 ) ;
 
 CREATE TABLE GUEST (
-    GuestID         VARCHAR(15)     NOT NULL,
-    GuestName       VARCHAR(15)     NOT NULL,
+    GuestID         CHAR(15)     NOT NULL,
+    GuestName       VARCHAR(30)     NOT NULL,
     PartySize       INT             NOT NULL,
     RoomCharges     INT,
     ServiceCharges  INT,
@@ -18,7 +18,51 @@ CREATE TABLE GUEST (
 ) ;
 
 CREATE TABLE EMPLOYEE (
-    EmployeeID      VARCHAR(15)     NOT NULL,
-    EmployeeName    VARCHAR(15)     NOT NULL,
+    EmployeeID      CHAR(15)     NOT NULL,
+    EmployeeName    VARCHAR(30)     NOT NULL,
     PRIMARY KEY (EmployeeID)
 ) ;
+
+CREATE TABLE SERVICE (
+    ServiceID      VARCHAR(15)     NOT NULL,
+    ServiceName    VARCHAR(30)     NOT NULL,
+    PRIMARY KEY (ServiceID)
+) ;
+
+CREATE TABLE LAUNDRY (
+    ServiceID      VARCHAR(15)     NOT NULL,
+    LaundryType VARCHAR(30) NOT NULL,
+    PRIMARY KEY (ServiceID),
+    CONSTRAINT FK_LaundryService FOREIGN KEY (ServiceID)
+    REFERENCES SERVICE(SERVICEID)
+) ;
+
+CREATE TABLE TRAVELDESK (
+    ServiceID      VARCHAR(15)     NOT NULL,
+    AreaOfInterest VARCHAR(50)     NOT NULL,
+    AppointmentDate TIMESTAMP      NOT NULL,
+    PRIMARY KEY (ServiceID),
+    CONSTRAINT FK_TravelDeskService FOREIGN KEY (ServiceID)
+    REFERENCES SERVICE(ServiceID)
+);
+
+CREATE TABLE RENTDEVICE (
+    ServiceID      VARCHAR(15)     NOT NULL,
+    DeviceRented     VARCHAR(30)     NOT NULL,
+    RentStart    TIMESTAMP       NOT NULL,
+    RentEnd      TIMESTAMP       NOT NULL,
+    PRIMARY KEY (ServiceID),
+    CONSTRAINT FK_DeviceRentalService FOREIGN KEY (ServiceID)
+    REFERENCES SERVICE(ServiceID)
+);
+
+CREATE TABLE BUSINESS (
+    ServiceID      VARCHAR(15)     NOT NULL,
+    ConNum     CHAR(3)     NOT NULL,
+    StartDate    TIMESTAMP       NOT NULL,
+    EndDate      TIMESTAMP       NOT NULL,
+    NumofRooms INT NOT NULL,
+    PRIMARY KEY (ServiceID),
+    CONSTRAINT FK_DeviceRentalService FOREIGN KEY (ServiceID)
+    REFERENCES SERVICE(ServiceID)
+);
