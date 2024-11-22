927053fa9772c203a64c54f2bd69336f63d44eaa
DanielMontoya72 <danipacmontoya@gmail.com>
2024-11-22 18:59:21 -0500

    not much

--- load5rows.sql
+++ load5rows.sql
@@ -3,8 +3,12 @@ Insert at least 5 rows data in each table via SQL statements.
 Write SELECT * for each table and do a SNIP or screen shot that shows your tables.
 */
 
+-- Loading 5 Unique ROOMs
 INSERT INTO ROOM VALUES(101, 100, 'Single Queen', 'Y', 2);
 INSERT INTO ROOM VALUES(202, 150, 'Single King', 'N', 2);
 INSERT INTO ROOM VALUES(303, 200, 'Double Queen', 'N', 4);
 INSERT INTO ROOM VALUES(404, 250, 'Double King', 'Y', 4);
 INSERT INTO ROOM VALUES(505, 500, 'Penthouse', 'Y', 6);
+
+-- Loading 5 Unique EMPLOYEEs
+INSERT INTO EMPLOYEE
