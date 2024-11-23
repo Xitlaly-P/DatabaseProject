5a14c69429b04baa9cd33772ae6764f2a8997af4
DanielMontoya72 <danipacmontoya@gmail.com>
2024-11-22 19:06:11 -0500

    5 employees ready to work!

--- load5rows.sql
+++ load5rows.sql
@@ -4,14 +4,23 @@ Write SELECT * for each table and do a SNIP or screen shot that shows your table
 */
 
 
----Guests
+-- GUESTs
 INSERT INTO GUEST VALUES('0000001', 'Angel Apellido', 3);
 INSERT INTO GUEST VALUES('0000002', 'Paula Ramos', 2);
 INSERT INTO GUEST VALUES('0000003', 'William George', 1);
 INSERT INTO GUEST VALUES('0000004', 'Benny Barnes', 4);
 INSERT INTO GUEST VALUES('0000005', 'Tiffany Li', 2);
+
+-- ROOMs
 INSERT INTO ROOM VALUES(101, 100, 'Single Queen', 'Y', 2);
 INSERT INTO ROOM VALUES(202, 150, 'Single King', 'N', 2);
 INSERT INTO ROOM VALUES(303, 200, 'Double Queen', 'N', 4);
 INSERT INTO ROOM VALUES(404, 250, 'Double King', 'Y', 4);
 INSERT INTO ROOM VALUES(505, 500, 'Penthouse', 'Y', 6);
+
+-- EMPLOYEEs
+INSERT INTO EMPLOYEE VALUES('1000001', 'Ydalis Zamora');
+INSERT INTO EMPLOYEE VALUES('1000002', 'Frederico Berryfaz');
+INSERT INTO EMPLOYEE VALUES('1000003', 'Oscar Init');
+INSERT INTO EMPLOYEE VALUES('1000004', 'William Werm');
+INSERT INTO EMPLOYEE VALUES('1000005', 'JG Quintel');
